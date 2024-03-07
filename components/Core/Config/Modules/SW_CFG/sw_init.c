dc88569f229b5fd141b237250594938e9a5f9afc
jervanENG <jervaneng.rob@gmail.com>
2024-03-07 10:46:43 +0100

    [APPWAVE] Release - V1.1.2 Safe Memory fixes

--- components/Core/Config/Modules/SW_CFG/sw_init.c
+++ components/Core/Config/Modules/SW_CFG/sw_init.c
@@ -15,7 +15,7 @@ void systemConfig()
 void taskConfig()
 {  
     mutex_create();
-    //create_task(task_analog,"Analog Task",4096,ANA_T_PERIOD,ANA_T_PRIOR);
+    create_task(task_analog,"Analog Task",4096,ANA_T_PERIOD,ANA_T_PRIOR);
     create_task(task_control,"Control Task",4096,CONTROL_T_PERIOD,CONTROL_T_PRIOR);
     create_task(task_wireless,"Wireless Task",16384,WIRELESS_T_PERIOD,WIRELESS_T_PRIOR);
 }
